f1542505c2305e7b9a468738a5f96c735e00406b
frank Onojaife <frankonojaifecareer@gmail.com>
2023-03-12 19:58:58 +0100

    All Task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
+/* eslint-disable */
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => {
+      response[1].value = `Error: ${response[1].reason.message}`;
+      delete response[1].reason;
+      return response;
+    });
+
 }
